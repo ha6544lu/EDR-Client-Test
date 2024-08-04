785635c8cd7b81593bc16ff1fb480f9f67f8e7ac
Hanna Lundström <hannaalsike@gmail.com>
2024-08-04 11:45:39 +0200

    Uppdatering

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,12 +1,25 @@
+/*BSD 2-Clause License
+
+Copyright 2005-present, OpenLayers Contributors All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+    Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+    Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**/
+
 import { Component, OnInit, ChangeDetectionStrategy, Input, ElementRef, EventEmitter, Output } from '@angular/core';
 import Map from 'ol/Map';
 import { Feature, View } from 'ol';
-import { Extent } from 'ol/extent';
+//import { Extent } from 'ol/extent';
 import Polygon from 'ol/geom/Polygon';
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
 import TileLayer from 'ol/layer/Tile';
-import { OSM } from 'ol/source';
+import OSM from 'ol/source/OSM';
 import Draw from 'ol/interaction/Draw';
 import { Fill, Stroke, Style } from 'ol/style';
 import { MousePositionComponent } from '../mouse-position/mouse-position.component';
@@ -25,6 +38,7 @@ export class MapComponent implements OnInit {
   @Input()
   map: Map = new Map();
   private draw: Draw | null = null;
+  //@ts-ignore
   private vectorLayer: VectorLayer<VectorSource> | null = null;
   @Output() rectangleDrawn = new EventEmitter<[number, number, number, number]>();
   @Output() pointDrawn = new EventEmitter<[number, number]>();
@@ -73,7 +87,7 @@ export class MapComponent implements OnInit {
       if (this.vectorLayer) {
         this.map.removeLayer(this.vectorLayer);
       }
-
+      //@ts-ignore
       this.vectorLayer = new VectorLayer({
         source: vectorSource,
       });
@@ -126,7 +140,7 @@ export class MapComponent implements OnInit {
     if (this.vectorLayer) {
       this.map.removeLayer(this.vectorLayer);
     }
-
+    //@ts-ignore
     this.vectorLayer = vector;
     this.map.addLayer(vector);
 
@@ -151,46 +165,6 @@ export class MapComponent implements OnInit {
       this.disableDraw();  // Disable drawing after the shape/point is selected
     });
   } 
-
- /* enableDraw(): void {
-    this.disableDraw(); // Disable any existing draw interactions
-
-    const source = new VectorSource();
-    const vector = new VectorLayer({
-      source: source,
-      style: new Style({
-        fill: new Fill({
-          color: 'rgba(255, 255, 255, 0.2)',
-        }),
-        stroke: new Stroke({
-          color: '#ffcc33',
-          width: 2,
-        }),
-      }),
-    });
-
-    if (this.vectorLayer) {
-      this.map.removeLayer(this.vectorLayer);
-    }
-
-    this.vectorLayer = vector;
-    this.map.addLayer(vector);
-
-    this.draw = new Draw({
-      source: source,
-      type: 'Polygon',
-    });
-
-    this.map.addInteraction(this.draw);
-
-    this.draw.on('drawend', (event: { feature: any; }) => {
-      const feature = event.feature;
-      const extent = feature.getGeometry().getExtent();
-      console.log('Bounding Box Extent:', extent);
-      this.rectangleDrawn.emit(extent as [number, number, number, number]);
-      this.disableDraw();  // Disable drawing after the first shape is completed
-    });
-  }**/
 
   private disableDraw(): void {
     if (this.draw) {
